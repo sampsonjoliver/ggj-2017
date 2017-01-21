e64bba88c7525d17daf8b86168342ea78f2fea23
Tom Wright <tom.w.wright@live.com>
2017-01-21 12:34:59 +1100

    Ejected project, set up basic fade in/out for background

---
+++ src/GameController.js
@@ -0,0 +1,42 @@
+import * as Phaser from 'phaser';
+
+import oceanPath from '../public/img/background.jpg';
+import ocean2Path from '../public/img/background2.jpg';
+
+export default class GameController {
+  constructor(divId: string, width: number, height: number) {
+    this.phaser = new Phaser.Game(width, height, Phaser.AUTO, divId, { preload: this.preload, create: this.create, update: this.update });
+  }
+
+  preload() {
+    // 'this' is in the context of the new phaser game
+    this.load.image('ocean', oceanPath);
+    this.load.image('ocean2', ocean2Path);
+  }
+
+  create() {
+    this.stage.backgroundColor = '#000000';
+
+    this.image = this.add.image(0, 0, 'ocean');
+
+    this.image.inputEnabled = true;
+    this.image.events.onInputDown.add(GameController.transition.bind(this, 'ocean2'));
+
+    this.image.alpha = 0;
+
+    this.add.tween(this.image).to( { alpha: 1 }, 2000).start();
+  }
+
+  update() {
+    // nothing to do on update...
+  }
+
+  static sequence(newBackground) {
+    var fadeOut = this.add.tween(this.image).to( { alpha: 0 }, 1000);
+    fadeOut.onComplete.add(() => this.image.loadTexture(newBackground));
+    var fadeIn = this.add.tween(this.image).delay(500).to( { alpha: 1 }, 2000);
+    fadeOut.chain(fadeIn);
+
+    fadeOut.start();
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GameController from './GameController';
 import './index.css';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+// start up phaser
+var phaser = new GameController('phaser', 800, 600);
